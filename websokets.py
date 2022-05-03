c1243a0d46d234c8d94ef14f11d08b9e8eb412a9
Mihaylova Yuliya <mihaylova.yuliyaa@gmail.com>
2022-05-03 22:06:35 +0300

    update with locks

--- websokets.py
+++ websokets.py
@@ -1,7 +1,19 @@
 import asyncio
+import time
+
 import websockets
-import random
 from threading import Thread
+import threading
+
+
+def receive_data(chart_updater):
+    i = 0
+    while True:
+        i += 1
+        time.sleep(3)
+        with chart_updater.conditional_variable:
+            chart_updater.conditional_variable.notifyAll()
+            chart_updater.data = i
 
 
 class ChartUpdater(Thread):
@@ -9,12 +21,13 @@ class ChartUpdater(Thread):
         super().__init__()
         self.start_server = websockets.serve(self.update_chart, address, port)
         self.data = None
+        self.conditional_variable = threading.Condition()
 
     async def update_chart(self, websocket, path):
         while True:
-            if self.data is None:
-                await websocket.send(str(random.randint(0, 50)))
-            await asyncio.sleep(3)
+            with self.conditional_variable:
+                self.conditional_variable.wait()
+            await websocket.send(str(self.data))
 
     def start(self) -> None:
         asyncio.get_event_loop().run_until_complete(self.start_server)
@@ -23,4 +36,8 @@ class ChartUpdater(Thread):
 
 if __name__ == "__main__":
     chart_updater = ChartUpdater("0.0.0.0", 8080)
+
+    thread_to_receive_data = Thread(target=receive_data, args=(chart_updater, ))
+    thread_to_receive_data.start()
+
     chart_updater.start()
